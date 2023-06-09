a30792bb4ead72d1aa9fb1e928521d56bdfe3f13
Daniel Rosero <dandan32099@gmail.com>
2023-06-09 17:23:53 -0500

    Agregando funcionalidad basica de busqueda

--- dinamo_project/dinamo_app_gerente/api.py
+++ dinamo_project/dinamo_app_gerente/api.py
@@ -1,4 +1,5 @@
 from . import models
+from django.db.models import Q
 from rest_framework import viewsets, permissions
 from . import serializer
 
@@ -7,30 +8,110 @@ class RolViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = serializer.RolSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term)
+            )
+        return queryset
+
 class SucursalViewSet(viewsets.ModelViewSet):
     queryset = models.Sucursal.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = serializer.SucursalSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term)
+            )
+        return queryset
 
 class EstadoViewSet(viewsets.ModelViewSet):
     queryset = models.Estado.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = serializer.EstadoSerializer
 
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term)
+            )
+        return queryset
+
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = models.Usuario.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = serializer.UsuarioSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(cedula__icontains=search_term) |
+                Q(nombre__icontains=search_term) |
+                Q(correo__icontains=search_term) |
+                Q(telefono__icontains=search_term) |
+                Q(direccion__icontains=search_term) |
+                Q(sucursal__nombre__icontains=search_term) |
+                Q(rol__nombre__icontains=search_term)
+            )
+        return queryset
 
 class ProveedorViewSet(viewsets.ModelViewSet):
     queryset = models.Proveedor.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = serializer.ProveedorSerializer
 
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term) |
+                Q(nombre__producto__icontains=search_term)
+            )
+        return queryset
+
 class AutoViewSet(viewsets.ModelViewSet):
     queryset = models.Auto.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = serializer.AutoSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(modelo__icontains=search_term) |
+                Q(año__icontains=search_term) |
+                Q(color__icontains=search_term) |
+                Q(precio_icontains=search_term)
+            )
+        return queryset
 
 class RepuestoViewSet(viewsets.ModelViewSet):
     queryset = models.Repuesto.objects.all()
