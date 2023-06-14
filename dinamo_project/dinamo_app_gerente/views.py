7fcc48cac5685bc6434d96612021475786b3735f
Daniel Rosero <dandan32099@gmail.com>
2023-06-13 23:22:59 -0500

    Merge con rama main, añadiendo funcionalidad de busqueda

--- dinamo_project/dinamo_app_gerente/views.py
+++ dinamo_project/dinamo_app_gerente/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse,JsonResponse
 from django.shortcuts import redirect, get_object_or_404
 from rest_framework.decorators import api_view 
 from .serializer import *
@@ -6,30 +5,3 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-
-@api_view(['POST'])
-def register(request):
-    try:
-        serializer = User_serializer(data = request.data)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    except BaseException:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['POST'])
-def register(request):
-    try:
-        serializer = Rol_serializer(data = request.data)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    except BaseException:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-def register(request):
-    try:
-        serializer = Sucursal_serializer(data = request.data)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    except BaseException:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
