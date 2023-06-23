c41297fdd334fcbfb5cb2d1d82d36eacef685670
Daniel Rosero <dandan32099@gmail.com>
2023-06-23 13:43:09 -0500

    Ready to deploy API

--- dinamo_project/dinamo_app_gerente/urls.py
+++ dinamo_project/dinamo_app_gerente/urls.py
@@ -1,20 +1,20 @@
 from rest_framework import routers
-from . import api
+from . import views
 
 router = routers.DefaultRouter()
 
-router.register('rol', api.RolViewSet, 'admin_rol')
-router.register('sucursal', api.SucursalViewSet, 'admin_sucursal')
-router.register('estado', api.EstadoViewSet, 'admin_estado')
-router.register('usuario', api.UsuarioViewSet, 'admin_usuario')
-router.register('proveedor', api.ProveedorViewSet, 'admin_proveedor')
-router.register('auto', api.AutoViewSet, 'admin_auto')
-router.register('cita', api.CitaViewSet, 'admin_cita')
-router.register('venta',api.VentaViewSet, 'admin_venta')
-router.register('pago', api.PagoViewSet, 'admin_pago')
-router.register('registaller', api.RegistroTallerViewSet, 'admin_registaller')
-router.register('repuesto', api.RepuestoViewSet, 'admin_repuesto')
-router.register('credito', api.CreditoViewSet, 'admin_credito')
-router.register('reporte', api.ReporteViewSet, 'admin_reporte')
+router.register('rol', views.RolViewSet, 'admin_rol')
+router.register('sucursal', views.SucursalViewSet, 'admin_sucursal')
+router.register('estado', views.EstadoViewSet, 'admin_estado')
+router.register('usuario', views.UsuarioViewSet, 'admin_usuario')
+router.register('proveedor', views.ProveedorViewSet, 'admin_proveedor')
+router.register('auto', views.AutoViewSet, 'admin_auto')
+router.register('cita', views.CitaViewSet, 'admin_cita')
+router.register('venta',views.VentaViewSet, 'admin_venta')
+router.register('pago', views.PagoViewSet, 'admin_pago')
+router.register('registaller', views.RegistroTallerViewSet, 'admin_registaller')
+router.register('repuesto', views.RepuestoViewSet, 'admin_repuesto')
+router.register('credito', views.CreditoViewSet, 'admin_credito')
+router.register('reporte', views.ReporteViewSet, 'admin_reporte')
 
 urlpatterns = router.urls
--- dinamo_project/dinamo_app_gerente/views.py
+++ dinamo_project/dinamo_app_gerente/views.py
@@ -1,7 +1,251 @@
-from django.shortcuts import redirect, get_object_or_404
-from rest_framework.decorators import api_view 
-from .serializer import *
-from rest_framework.response import Response
-from rest_framework import status
+from . import models
+from django.db.models import Q
+from rest_framework import viewsets, permissions, generics
+from . import serializer
+import cloudinary.uploader
 
-# Create your views here.
+class RolViewSet(viewsets.ModelViewSet):
+    queryset = models.Rol.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.RolSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term)
+            )
+        return queryset
+
+class SucursalViewSet(viewsets.ModelViewSet):
+    queryset = models.Sucursal.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.SucursalSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term)
+            )
+        return queryset
+
+class EstadoViewSet(viewsets.ModelViewSet):
+    queryset = models.Estado.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.EstadoSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term)
+            )
+        return queryset
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = models.Usuario.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.UsuarioSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(cedula__icontains=search_term) |
+                Q(nombre__icontains=search_term) |
+                Q(correo__icontains=search_term) |
+                Q(telefono__icontains=search_term) |
+                Q(direccion__icontains=search_term) |
+                Q(sucursal__nombre__icontains=search_term) |
+                Q(rol__nombre__icontains=search_term)
+            )
+        return queryset
+
+class ProveedorViewSet(viewsets.ModelViewSet):
+    queryset = models.Proveedor.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.ProveedorSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre__icontains=search_term) |
+                Q(tipo_producto__incontains=search_term) |
+                Q(nombre_producto__icontains=search_term)
+            )
+        return queryset
+
+class AutoViewSet(viewsets.ModelViewSet):
+    queryset = models.Auto.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.AutoSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(modelo__icontains=search_term) |
+                Q(año__icontains=search_term) |
+                Q(color__icontains=search_term) |
+                Q(precio_icontains=search_term)
+            )
+        return queryset
+
+class RepuestoViewSet(viewsets.ModelViewSet):
+    queryset = models.Repuesto.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.RepuestoSerializer
+    
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(articulo__icontains=search_term) |
+                Q(sucursal__nombre__icontains=search_term) |
+                Q(proveedor__nombre__icontains=search_term)
+            )
+        return queryset
+
+
+
+class CitaViewSet(viewsets.ModelViewSet):
+    queryset = models.Cita.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.CitaSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(fecha__icontains=search_term) |
+                Q(nombre_empleado__icontains=search_term) |
+                Q(nombre_cliente__icontains=search_term)
+            )
+        return queryset
+
+class VentaViewSet(viewsets.ModelViewSet):
+    queryset = models.Venta.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.VentaSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(nombre_vendedor__icontains=search_term) |
+                Q(fecha_venta__icontains=search_term)
+            )
+        return queryset
+
+class PagoViewSet(viewsets.ModelViewSet):
+    queryset = models.Pago.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.PagoSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(cantidad_pago__icontains=search_term) |
+                Q(nombre_cliente__icontains=search_term) |
+                Q(id_venta__id__icontains=search_term)
+            )
+        return queryset
+
+class RegistroTallerViewSet(viewsets.ModelViewSet):
+    queryset = models.RegistroTaller.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.RegistroTallerSerializer
+
+        #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(placa_auto__icontains=search_term) |
+                Q(estado__icontains=search_term) |
+                Q(fecha_ingreso__icontains=search_term)
+            )
+        return queryset
+
+class CreditoViewSet(viewsets.ModelViewSet):
+    queryset = models.Credito.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializer.CreditoSerializer
+
+    #Funcion que recibe los parametros de una busqueda a traves de la URL y realiza la peticion correspondiente a la BD, para filtrar los datos y mostrarlos en pantalla 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(monto_pagado__icontains=search_term) |
+                Q(estado_pago__icontains=search_term) |
+                Q(cedula_cliente__nombre__icontains=search_term)
+            )
+        return queryset
+    
+class ReporteViewSet(viewsets.ModelViewSet):
+    queryset = models.Reporte.objects.all()
+    serializer_class = serializer.ReporteSerializer
+
+    def pdf_create(self, serializer):
+        archivo_pdf = self.request.get['archivo_pdf']
+        upload_result = cloudinary.uploader.upload(archivo_pdf)
+        serializer.save(archivo_pdf=upload_result['url'])
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', '')
+        
+        if search_term:
+            queryset = queryset.filter(
+                Q(titulo__icontains=search_term) |
+                Q(rol__nombre__icontains=search_term)
+            )
+        return queryset
