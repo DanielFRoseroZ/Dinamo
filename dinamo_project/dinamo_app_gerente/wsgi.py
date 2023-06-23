dea91fb32d20b1cb5c11b6e416b206a9d43e7ad2
Daniel Rosero <dandan32099@gmail.com>
2023-06-23 13:57:20 -0500

    Ready to deploy API

--- dinamo_project/dinamo_app_gerente/wsgi.py
+++ dinamo_project/dinamo_app_gerente/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dinamo.dinamo_project.dinamo_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dinamo_project.settings')
 
 application = get_wsgi_application()
